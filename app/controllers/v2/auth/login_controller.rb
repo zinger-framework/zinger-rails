c891299c86b6f8e8cd82ec291a9788e8bf1b9628
Logesh Dinakaran <ddlogesh@gmail.com>
2020-09-30 19:23:53 +0530

    Auth - Login - Initial commit

---
+++ app/controllers/v2/auth/login_controller.rb
@@ -0,0 +1,18 @@
+class V2::Auth::LoginController < V2::AuthController
+  def login
+    
+  end
+
+  private
+  def login_with_password
+    
+  end
+
+  def login_with_otp
+    
+  end
+
+  def login_with_google
+    
+  end
+end
