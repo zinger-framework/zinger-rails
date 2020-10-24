6dcbf9ac725dc0651859326f85d96fe4174a4a61
Logesh Dinakaran <ddlogesh@gmail.com>
2020-10-24 21:26:29 +0530

    PlatformConfig - Added otp, password length properties

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ApplicationRecord
-  PASSWORD_MIN_LENGTH = 6
-  OTP_LENGTH = 6
+  PASSWORD_MIN_LENGTH = PlatformConfig['password_min_length']
+  OTP_LENGTH = PlatformConfig['otp_length']
   EMAIL_REGEX = /\S+@\S+\.[a-z]+/i
   MOBILE_REGEX = /^[0-9]{10}$/
   STATUSES = { 'ACTIVE' => 1, 'BLOCKED' => 2 }
