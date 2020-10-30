04e67e8f976fae2f489716e652d6f1ac1644ec8b
harshavardhanp <harshavardhan.zodiac@gmail.com>
2020-10-30 23:35:46 +0530

    Customer UI: Refactored customer_controller

--- app/controllers/v2/customer_controller.rb
+++ app/controllers/v2/customer_controller.rb
@@ -6,20 +6,17 @@ class V2::CustomerController < ApiController
   def update_profile
     if params['name'].blank?
       render status: 400, json: { success: false, message: I18n.t('profile.update_failed'),
-                                  reason: { name: [ I18n.t('validation.required', param: 'Name') ] } }
+        reason: { name: [ I18n.t('validation.required', param: 'Name') ] } }
       return
     end
 
     Customer.current.update(name: params['name'])
     if Customer.current.errors.any?
-      render status: 400, json: { success: false, message: I18n.t('profile.update_failed'),
-                                  reason: Customer.current.errors.messages }
+      render status: 400, json: { success: false, message: I18n.t('profile.update_failed'), reason: Customer.current.errors.messages }
       return
     end
     render status: 200, json: { success: true, message: I18n.t('profile.update_success'),
-                                data: Customer.current.as_json('profile') }
+      data: Customer.current.as_json('profile') }
   end
 end
-
-
 
