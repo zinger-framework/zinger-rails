a772c8d4f9bd3e1f8cb721237332b6ac4a946d41
Logesh Dinakaran <ddlogesh@gmail.com>
2020-11-07 19:09:06 +0530

    UI - Added shop & shop detail table

---
+++ app/models/shop.rb
@@ -0,0 +1,20 @@
+class Shop < ApplicationRecord
+  STATUSES = { 'ACTIVE' => 1, 'BLOCKED' => 2 }
+
+  default_scope { where(status: STATUSES['ACTIVE'], deleted: false) }
+  searchkick word_start: ['name'], locations: ['location'], default_fields: ['status', 'deleted']
+
+  has_one :shop_detail
+
+  def search_data
+    { name: self.name, location: { lat: self.lat, lon: self.lng }, tag: self.tag }
+  end
+
+  def as_json purpose = nil
+    case purpose
+    when 'ui_shop'
+      return { 'id' => self.id, 'name' => self.name, 'icon' => self.icon, 'tags' => self.tag.split(' ') }
+        .merge(self.shop_detail.as_json('ui_shop_detail'))
+    end
+  end
+end
