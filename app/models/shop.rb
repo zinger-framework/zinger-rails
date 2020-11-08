b1f0e46abeb997e0a31e754052677ca9d782fa5e
Logesh Dinakaran <ddlogesh@gmail.com>
2020-11-08 12:21:59 +0530

    Admin - Added Shop APIs

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -13,11 +13,16 @@ class Shop < ApplicationRecord
   def as_json purpose = nil
     case purpose
     when 'ui_shop'
-      return { 'id' => self.id, 'name' => self.name, 'icon' => self.icon, 'tags' => self.tags.split(' '),
-        'area' => self.shop_detail.address['area'] }
+      return { id: self.id, name: self.name, icon: self.icon, tags: self.tags.split(' '), area: self.shop_detail.address['area'] }
     when 'ui_shop_detail'
-      return { 'id' => self.id, 'name' => self.name, 'icon' => self.icon, 'tags' => self.tags.split(' ') }
-        .merge(self.shop_detail.as_json('ui_shop_detail'))
+      return { id: self.id, name: self.name, icon: self.icon, tags: self.tags.split(' ') }.merge(self.shop_detail.as_json('ui_shop_detail'))
+    when 'admin_shop'
+      return { id: self.id, name: self.name, icon: self.icon, tags: self.tags.split(' '), lat: self.lat, lng: self.lng, status: self.status,
+        deleted: self.deleted, updated_at: self.updated_at.in_time_zone(PlatformConfig['time_zone']).strftime('%d-%m-%Y %H:%M') }
     end
+  end
+
+  def self.fetch_by_id id
+    Core::Redis.fetch(Core::Redis::SHOP_BY_ID % { id: id }, { type: Shop }) { Shop.find_by_id(id) }
   end
 end
--- app/models/shop_detail.rb
+++ app/models/shop_detail.rb
@@ -10,6 +10,11 @@ class ShopDetail < ApplicationRecord
       return { 'address' => self.address, 'landline' => self.landline, 'mobile' => self.mobile, 'cover_photos' => self.cover_photos,
         'opening_time' => opening_time.strftime('%I:%M %p'), 'closing_time' => closing_time.strftime('%I:%M %p'),
         'open_now' => opening_time.strftime('%H:%M') <= time && time < closing_time.strftime('%H:%M') }
+    when 'admin_shop_detail'
+      return { 'address' => self.address, 'landline' => self.landline, 'mobile' => self.mobile, 'cover_photos' => self.cover_photos,
+        'opening_time' => self.opening_time.in_time_zone(PlatformConfig['time_zone']).strftime('%I:%M %p'), 
+        'closing_time' => self.closing_time.in_time_zone(PlatformConfig['time_zone']).strftime('%I:%M %p'),
+        'payment' => self.payment, 'meta' => self.meta }
     end
   end
 end
