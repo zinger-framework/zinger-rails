335329395870bf2f97b4761100c55f669991975e
Logesh Dinakaran <ddlogesh@gmail.com>
2021-01-30 18:35:26 +0530

    Removed unused files

--- public/vendors/scripts/advanced-components.js
+++
@@ -1,5 +0,0 @@
-// Switchery
-var elems = Array.prototype.slice.call(document.querySelectorAll('.switch-btn'));
-$('.switch-btn').each(function() {
-	new Switchery($(this)[0], $(this).data());
-});
--- public/vendors/scripts/dashboard.js
+++ public/vendors/scripts/dashboard.js
@@ -1,4 +1,4 @@
-var options5 = {
+var txn_options = {
 	chart: {
 		height: 350,
 		type: 'bar',
@@ -94,10 +94,9 @@ var options5 = {
 		}
 	}
 }
-var chart5 = new ApexCharts(document.querySelector("#chart5"), options5);
-chart5.render();
+var txn_chart = new ApexCharts(document.querySelector("#txn_chart"), txn_options);
+txn_chart.render();
 
-// datatable init
 $('document').ready(function(){
 	$('.data-table').DataTable({
 		scrollCollapse: true,
@@ -122,3 +121,63 @@ $('document').ready(function(){
 		},
 	});
 });
+
+var reg_options = {
+	series: [{
+		name: 'Users',
+		data: [4, 3, 10, 9, 29, 19, 22, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5]
+	}],
+	chart: {
+		height: 350,
+		type: 'line',
+		toolbar: {
+			show: false,
+		}
+	},
+	grid: {
+		show: false,
+		padding: {
+			left: 0,
+			right: 0
+		}
+	},
+	stroke: {
+		width: 7,
+		curve: 'smooth'
+	},
+	xaxis: {
+		type: 'datetime',
+		categories: ['1/11/2020', '2/11/2020', '3/11/2020', '4/11/2020', '5/11/2020', '6/11/2020', '7/11/2020', '8/11/2020', '9/11/2020', '10/11/2020', '11/11/2020', '12/11/2020', '1/11/2021', '2/11/2021', '3/11/2021','4/11/2021' ,'5/11/2021' ,'6/11/2021'],
+	},
+	fill: {
+		type: 'gradient',
+		gradient: {
+			shade: 'dark',
+			gradientToColors: [ '#1b00ff'],
+			shadeIntensity: 1,
+			type: 'horizontal',
+			opacityFrom: 1,
+			opacityTo: 1,
+			stops: [0, 100, 100, 100]
+		},
+	},
+	markers: {
+		size: 4,
+		colors: ["#FFA41B"],
+		strokeColors: "#fff",
+		strokeWidth: 2,
+		hover: {
+			size: 7,
+		}
+	},
+	yaxis: {
+		min: -10,
+		max: 40,
+		title: {
+			text: 'Users',
+		},
+	}
+};
+
+var reg_chart = new ApexCharts(document.querySelector("#reg_chart"), reg_options);
+reg_chart.render();
