879d56e3accf587201b5c2b365e555f21da36e82
Logesh Dinakaran <ddlogesh@gmail.com>
2021-06-06 00:37:24 +0530

    Added UTC time in filter

--- lib/validate_param/shop.rb
+++ lib/validate_param/shop.rb
@@ -1,12 +1,12 @@
 class ValidateParam::Shop
-  DEFAULT_START_TIME = Time.strptime('2020-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+  DEFAULT_START_TIME = Time.find_zone(PlatformConfig['time_zone']).strptime('2020-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
 
   def self.load_conditions params
-    conditions, start_time, end_time = {}, DEFAULT_START_TIME, Time.now
+    conditions, start_time, end_time = {}, DEFAULT_START_TIME, Time.now.in_time_zone(PlatformConfig['time_zone'])
 
     if params['start_time'].present?
       begin
-        start_time = Time.strptime(params['start_time'], '%Y-%m-%d %H:%M:%S')
+        start_time = Time.find_zone(PlatformConfig['time_zone']).strptime(params['start_time'], '%Y-%m-%d %H:%M:%S')
       rescue ArgumentError => e
         return I18n.t('validation.invalid_time_format')
       end
@@ -14,7 +14,7 @@ class ValidateParam::Shop
 
     if params['end_time'].present?
       begin
-        end_time = Time.strptime("#{params['end_time']}.999999999", '%Y-%m-%d %H:%M:%S.%N')
+        end_time = Time.find_zone(PlatformConfig['time_zone']).strptime("#{params['end_time']}.999999999", '%Y-%m-%d %H:%M:%S.%N')
       rescue ArgumentError => e
         return I18n.t('validation.invalid_time_format')
       end
@@ -22,7 +22,7 @@ class ValidateParam::Shop
 
     if params['start_time'].present? || params['end_time'].present?
       return I18n.t('validation.invalid_time_range') if start_time > end_time
-      conditions['created_at'] = start_time..end_time
+      conditions['created_at'] = start_time.utc..end_time.utc
     end
 
     if params['statuses'].class == String
