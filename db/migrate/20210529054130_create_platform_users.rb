03646c39c031bb508518c885d32570bd5c3d4a00
Logesh Dinakaran <ddlogesh@gmail.com>
2021-06-06 13:27:09 +0530

    Modified migration

--- db/migrate/20210529054130_create_platform_users.rb
+++ db/migrate/20210529054130_create_platform_users.rb
@@ -1,7 +1,7 @@
 class CreatePlatformUsers < ActiveRecord::Migration[6.0]
   DEFAULT_PASSWORD = 'admin123'
 
-  def change
+  def up
     create_table :platform_users do |t|
       t.string :name
       t.string :email
@@ -17,5 +17,9 @@ class CreatePlatformUsers < ActiveRecord::Migration[6.0]
     
     PlatformUser.create!(name: 'Admin', email: 'admin@zinger.pw', password: DEFAULT_PASSWORD, password_confirmation: DEFAULT_PASSWORD, 
       status: PlatformUser::STATUSES['ACTIVE'])
+  end
+
+  def down
+    drop_table :platform_users
   end
 end
